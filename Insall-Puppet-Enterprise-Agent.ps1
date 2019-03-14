35c31827b4037f4ec2db68e595db16fd54f8024d
Daniel Wittenberg <dan.wittenberg@thinkahead.com>
2019-03-14 12:15:18 -0500

    Update Insall-Puppet-Enterprise-Agent.ps1

--- Insall-Puppet-Enterprise-Agent.ps1
+++ Insall-Puppet-Enterprise-Agent.ps1
@@ -1,5 +1,11 @@
+param(
+  [string] $environment = "production",
+  [string] $role = "role::base_windows_role",
+  [string] $master = "pe-2019-m01.aheadaviation.local",
+)
+
 [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
 [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 $webClient=New-Object System.Net.WebClient
-$webClient.DownloadFile('https://pe-2019-m01.aheadaviation.local:8140/packages/current/install.ps1', 'install.ps1')
-.\install.ps1 custom_attributes:challengePassword=sUperSecret extension_requests:pp_environment=production extension_requests:pp_role=role::base_windows_role
+$webClient.DownloadFile('https://${master}:8140/packages/current/install.ps1', 'install.ps1')
+.\install.ps1 custom_attributes:challengePassword=sUperSecret extension_requests:pp_environment=${environment} extension_requests:pp_role=${role}
